abb292e3c4783c273644ae40e7f6ba22c608e714
dadatoa <f0a202@outlook.com>
2026-06-07 09:02:12 +0700

    update cattpuccin tmux script

--- run_once_install_catppuccin-tmux.sh
+++ run_once_install_catppuccin-tmux.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-mkdir -p ~/.tmux/plugins/catppuccin && git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.tmux/plugins/catppuccin/tmux
+mkdir -p ~/.tmux/plugins/catppuccin && git clone -b v2.3.0 https://github.com/catppuccin/tmux.git ~/.tmux/plugins/catppuccin/tmux
 
